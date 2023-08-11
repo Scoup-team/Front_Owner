73992b38375b95550ce9b1f17fc38b0423d15c08
godori1012 <rhdmstjrjdi@naver.com>
2023-08-11 17:00:10 +0900

    💄 MainPage 이름 색상 수정

--- owner/screens/MainPage.js
+++ owner/screens/MainPage.js
@@ -20,7 +20,8 @@ const MainPage = ({ navigation }) => {
       <Text style={styles.title}>마이 페이지</Text>
       <View style={styles.usersection}>
         <Image source={coffee} style={styles.coffee} />
-        <Text style={styles.nickname}>고은서 사장님</Text>
+        <Text style={styles.name}>고은서</Text>
+        <Text style={styles.nickname}> 사장님</Text>
       </View>
       <View style={styles.section}>
         <Text style={styles.topMenu}>개인 정보 수정</Text>
@@ -66,7 +67,7 @@ const styles = StyleSheet.create({
     textAlign: "center",
     fontSize: 20,
     fontStyle: "normal",
-    fontWeight: 700,
+    fontWeight: "700",
     marginTop: 70,
     marginBottom: 50,
   },
@@ -82,14 +83,22 @@ const styles = StyleSheet.create({
     width: 35,
     height: 35,
   },
+  name: {
+    color: "#6E85B7",
+    textAlign: "left",
+    fontSize: 28,
+    fontStyle: "normal",
+    fontWeight: "700",
+    marginLeft: 10,
+  },
 
   nickname: {
     color: "#5A5858",
     textAlign: "left",
     fontSize: 28,
     fontStyle: "normal",
-    fontWeight: 700,
-    marginLeft: 10,
+    fontWeight: "700",
+    // marginLeft: 10,
   },
 
   topMenu: {
@@ -97,7 +106,7 @@ const styles = StyleSheet.create({
     textAlign: "left",
     fontSize: 20,
     fontStyle: "normal",
-    fontWeight: 700,
+    fontWeight: "700",
     marginBottom: 34,
     marginLeft: 30.51,
   },
@@ -107,7 +116,7 @@ const styles = StyleSheet.create({
     textAlign: "left",
     fontSize: 20,
     fontStyle: "normal",
-    fontWeight: 700,
+    fontWeight: "700",
     marginBottom: 34,
     marginLeft: 30.51,
   },
